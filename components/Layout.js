37b31781e794569a336d0d39355a8ad1ad0c4e2b
Dameon Sumpton <Dsumpton@hotmail.co.uk>
2018-11-19 16:37:58 +0000

    added setup for testing

--- components/Layout.js
+++ components/Layout.js
@@ -1,4 +1,5 @@
 import { Component } from 'react'
+import Footer from '../components/Footer'
 import '../scss/application.scss'
 
 class Layout extends Component {
---
+++ test/footer.test.js
@@ -0,0 +1,10 @@
+import Footer from '../../components/Footer'
+
+describes ('<Footer />', () => {
+  it('it renders correctly', () => {
+    const component = shallow(
+      <Footer />
+    )
+    expect(component).toMatchSnapshot()
+  })
+})
